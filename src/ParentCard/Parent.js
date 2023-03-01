fdd4d40e7a50a9101ff3958c8f52c5d101d4b19f
Angela Wirth <angiewirth92@gmail.com>
2023-03-01 08:16:51 -0700

    Reduce array size using slice

--- src/ParentCard/Parent.js
+++ src/ParentCard/Parent.js
@@ -1,14 +1,26 @@
 import React from "react";
 import Card from "../Card/Card";
+import './Parent.css'
 
 const Parent = ({fishies}) => {
-  const fish = fishies.map(fish => {
+  const selectFish = fishies.slice(0,9)
+  const fish = selectFish.map(fish => {
+    return (
     <Card
-
+      // image={fishies["Image Gallery"]}
+      species={fish["Species Name"]}
+      protein={fish.protein}
+      sodium={fish.sodium}
+      key={fish.index}
     />
+    )
   })
+
+  return (
+    <div className='card-container'>
+      {fish}
+    </div>
+  )
 }
-
-
 
 export default Parent
