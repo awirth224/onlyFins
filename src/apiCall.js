aa26498cc85bf5b49ac74c07a4b761ef9ebe215a
Angela Wirth <angiewirth92@gmail.com>
2023-03-01 14:05:12 -0700

    Add fetch for specific endpoint

--- src/apiCall.js
+++ src/apiCall.js
@@ -1,6 +1,12 @@
-const getFish = () => {
+const getAllFish = () => {
   return fetch('https://www.fishwatch.gov/api/species')
     .then(response => response.json())
 }
 
-export default getFish
+const getSingleFish = (path) => {
+  const endpoint = path.split("/")[1]
+  return fetch(`https://www.fishwatch.gov/api/species/${endpoint}`)
+    .then(response => response.json())
+}
+
+export {getAllFish, getSingleFish}
