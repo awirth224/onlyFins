6d64f80068c0830aaf2b0d6f9e0e8bd75ce8f2e8
Angela Wirth <angiewirth92@gmail.com>
2023-03-02 14:55:21 -0700

    Add classname to h3s for styling

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,13 +4,13 @@ import { NavLink } from 'react-router-dom'
 
 const Card = ({src, alt, species, cal, protein}) => {
   return (
-    <NavLink to={`/${species}`}>
+    <NavLink to={`/${species}`} className='link'>
       <div className='card'>
         <img src={src} alt={alt}/>
         <div className="info-container">
           <h2>{species}</h2>
-          <h3>Calories: {cal}</h3>
-          <h3>Protein: {protein}</h3>
+          <h3 className="info">Calories: {cal}</h3>
+          <h3 className="info">Protein: {protein}</h3>
         </div>
       </div>
     </NavLink>
