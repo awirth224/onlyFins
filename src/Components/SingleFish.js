6cded00c2ef69480aeb355fda6ff0bc695f96322
Angela Wirth <angiewirth92@gmail.com>
2023-03-02 14:56:31 -0700

    Add link to illustration if no image found

--- src/Components/SingleFish.js
+++ src/Components/SingleFish.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Nutrition from "./Nutrition";
-import '../CSS/Fish.css'
+import '../CSS/SingleFish.css'
 
 const Fish = (props) => {
   const findFish = (fishName) => {
     const found = props.fishies.find(fish => fish.name === fishName)
       return (
         <div className="single-fish-container">
-          <img src={found.gallery[0].src} alt={found.gallery[0].alt}/>
-          <Nutrition
-          props={found}
-          />
+          <div className="img-info-container">
+            <img src={found.gallery[0].src || found.illustration.src} alt={found.gallery[0].alt || found.illustration.alt}/>
+            <Nutrition
+            props={found}
+            />
+          </div>
+          <div className="btn-container">
+            <button className="home-btn">Home</button>
+          </div>
         </div>
       )
   }
