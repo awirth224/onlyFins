79ef60ac983b342309f7209cc80a71df4ddea7ef
Angela Wirth <angiewirth92@gmail.com>
2023-03-01 20:11:42 -0700

    Import NavLink

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,16 +1,19 @@
 import React from "react";
 import './Card.css'
+import { NavLink } from 'react-router-dom'
 
 const Card = ({src, alt, species, cal, protein}) => {
   return (
-    <div className='card'>
-      <img src={src} alt={alt}/>
-      <div className="info-container">
-        <h2>{species}</h2>
-        <h3>Calories: {cal}</h3>
-        <h3>Protein: {protein}</h3>
+    <NavLink to={`/${species}`}>
+      <div className='card'>
+        <img src={src} alt={alt}/>
+        <div className="info-container">
+          <h2>{species}</h2>
+          <h3>Calories: {cal}</h3>
+          <h3>Protein: {protein}</h3>
+        </div>
       </div>
-    </div>
+    </NavLink>
   )
  }
  
