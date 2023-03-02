79dd48dad5fde8b053332cc3dead99a1a622af3a
Angela Wirth <angiewirth92@gmail.com>
2023-03-02 14:55:57 -0700

    Add NA if no value given

--- src/Components/Nutrition.js
+++ src/Components/Nutrition.js
@@ -7,12 +7,12 @@ const Nutrition = ({props}) => {
     <div className="nutrition-container">
       <h3>Serving: {props.serving}</h3>
       <h3>Calories: {props.calories}</h3>
-      <h3>Carbohydrates: {props.carbohydrate}</h3>
+      <h3>Carbohydrates: {props.carbohydrate || "N/A"}</h3>
       <h3>Cholesterol: {props.cholesterol}</h3>
       <h3>Fat: {props.fat}</h3>
       <h3>Fiber: {props.fiber}</h3>
-      <h3>Sodium:{props.sodium}</h3>
-      <h3>sugar:{props.sugar}</h3>
+      <h3>Sodium: {props.sodium}</h3>
+      <h3>Sugar: {props.sugar}</h3>
     </div>
 
 
