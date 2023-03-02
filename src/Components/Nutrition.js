892d110fa9f54a1ce5435fca3f43e5bcdff57479
Angela Wirth <angiewirth92@gmail.com>
2023-03-02 14:15:30 -0700

    Add class name to div for styling

--- src/Components/Nutrition.js
+++ src/Components/Nutrition.js
@@ -2,17 +2,17 @@ import React from "react";
 import '../CSS/Nutrition.css';
 
 
-const Nutrition = ({serving, calories, carbohydrate, cholesterol, fat, fiber, sodium, sugar}) => {
+const Nutrition = ({props}) => {
   return(
     <div className="nutrition-container">
-      <h3>Serving: {serving}</h3>
-      <h3>Calories: {calories}</h3>
-      <h3>Carbohydrates: {carbohydrate}</h3>
-      <h3>Cholesterol: {cholesterol}</h3>
-      <h3>Fat: {fat}</h3>
-      <h3>Fiber: {fiber}</h3>
-      <h3>Sodium:{sodium}</h3>
-      <h3>sugar:{sugar}</h3>
+      <h3>Serving: {props.serving}</h3>
+      <h3>Calories: {props.calories}</h3>
+      <h3>Carbohydrates: {props.carbohydrate}</h3>
+      <h3>Cholesterol: {props.cholesterol}</h3>
+      <h3>Fat: {props.fat}</h3>
+      <h3>Fiber: {props.fiber}</h3>
+      <h3>Sodium:{props.sodium}</h3>
+      <h3>sugar:{props.sugar}</h3>
     </div>
 
 
