c9615482dea92ab606c64c208b762fa67f3292fd
Angela Wirth <angiewirth92@gmail.com>
2023-03-01 20:13:18 -0700

    Create method to find the fish that matches the one a user clicks on via match.param

--- src/SingleFish/Fish.js
+++ src/SingleFish/Fish.js
@@ -2,18 +2,28 @@ import React from "react";
 import MiscInfo from "./MiscInfo";
 import Nutrition from "./Nutrition";
 
-const Fish = ({fishies}) => {
+const Fish = (props) => {
+  const findFish = (fishName) => {
+    const found = props.fishies.find(fish => fish.name === fishName)
+      return (
+        <div>
+          <Nutrition
+           serving={found.serving}
+           calories={found.calories}
+           carbohydrates={found.carbohydrates}
+           cholesterol={found.cholesterol}
+           fat={found.fat}
+           fiber={found.fiber}
+           sodium={found.sodium}
+           sugar={found.sugar}
+          />
+        </div>
+      )
+  }
+  
   return (
-    <div>
-      <div>
-        <Nutrition />
-        <MiscInfo />
-      </div>
-    </div>
+    findFish(props.fish)
   )
-
 }
-
-
 
 export default Fish
