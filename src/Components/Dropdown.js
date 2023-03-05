3936b8a1491632b6bb3cd773394ef64b26cc6678
Angela Wirth <angiewirth92@gmail.com>
2023-03-05 09:47:35 -0700

    Change props to onChange

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,14 +1,13 @@
 import React from "react";
 import '../CSS/DropDown.css'
 
-const DropDown = ({props}) => {
-
+const DropDown = (onChange) => {
 
   return (
     <div className='sort-container'>
       <div className='dropdown'>
         <label htmlFor="sort" className='label'></label>
-        <select id="sort" name="sort" className='select' defaultValue={'select'}>
+        <select id="sort" name="sort" className='select' defaultValue={'select'} value=''>
           <option value="select">Select</option>
           <option value="C-HL">Calories: High to Low</option>
           <option value="C-LH">Calories: Low to High</option>
