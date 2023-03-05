2ea9a1c75fb1aebd5d3ddae2f406445c8b7f9cea
Angela Wirth <angiewirth92@gmail.com>
2023-03-05 16:21:56 -0700

    Add the species name to the single fish page

--- src/Components/Nutrition.js
+++ src/Components/Nutrition.js
@@ -5,6 +5,7 @@ import '../CSS/Nutrition.css';
 const Nutrition = ({props}) => {
   return(
     <div className="nutrition-container">
+      <h3 className="name">{props.name}</h3>
       <h3>Serving: {props.serving || "N/A"}</h3>
       <h3>Calories: {props.calories || "N/A"}</h3>
       <h3>Carbohydrates: {props.carbohydrate || "N/A"}</h3>
