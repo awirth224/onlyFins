ea392885edbf29f4d305da5e0892d9808201b9c6
Angela Wirth <angiewirth92@gmail.com>
2023-03-05 12:05:56 -0700

    Remove src property from example data to try and circumvent error- did not work.

--- cypress/e2e/dashboard.cy.js
+++ cypress/e2e/dashboard.cy.js
@@ -1,9 +1,10 @@
 beforeEach(() => {
   cy.visit('http://localhost:3000/')
-  cy.intercept('GET', 'https://www.fishwatch.gov/api/species',  )
+  cy.intercept('GET', 'https://www.fishwatch.gov/api/species')
 })
 
-describe('template spec', () => {
-  it('passes', () => {
+describe('dashboard of onlyFins', () => {
+  it('should have a title, display all fish data and have a button to sort fish', () => {
+    cy.get('header[class="App-header"]')
   })
 })
